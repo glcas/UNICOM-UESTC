7fdf8eee41b60af63cd0c8cedebe0d2da52889aa
DicHac <glcas@qq.com>
2020-02-22 20:50:19 +0800

    for clear notices

--- logout.py
+++ logout.py
@@ -1,7 +1,7 @@
 # 需与login脚本放在同一目录下
 # 最后打包成exe，打包前删除生成日志文件相关代码
 
-import time
+from time import ctime
 import requests
 
 
@@ -16,11 +16,11 @@ def main():
             'Host':
             '221.10.255.233:8088',
             'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
             'Accept':
             '*/*',
             'Accept-Language':
-            'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'zh-CN,zh;q=0.9',
             'Accept-Encoding':
             'gzip, deflate',
             'Connection':
@@ -28,17 +28,19 @@ def main():
             'Referer':
             'http://221.10.255.233:8088//LoginServlet',
             'Cookie':
-            'JSESSIONID={}; LOGIN_USER_NAME_COOKIE=02802118659; LOGIN_PASSWORD_COOKIE=184312'
-            .format(JSESSIONID)
+            'LOGIN_USER_NAME_COOKIE=02802118659; LOGIN_PASSWORD_COOKIE=184312; JSESSIONID={}'
+            .format(JSESSIONID),
+            'DNT':
+            '1'
         }
         logout = requests.get(logoutUrl, headers=myHeaders)
-        log.write('{}'.format(time.ctime()))
+        log.write('{}'.format(ctime()))
         if logout.text == '[SUCCESS]':
             log.write('  logout success\n')
         else:
             log.write('  logout failed without request error\n')
     except requests.exceptions.RequestException:
-        log.write('{}  logout failed\n'.format(time.ctime()))
+        log.write('{}  logout failed\n'.format(ctime()))
     finally:
         log.close()
 
